97a26a5f969044a91df58eadc4ffb529f3f8b5dc
KattyVT <92695956+KattyVT@users.noreply.github.com>
2021-10-24 16:41:55 +0200

    Added date-time function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[date.getDay()];
+let hour = date.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = date.getMinutes(); 
+if (minutes < 10 ) {
+  minutes = `0${minutes}`;
+}
+  
+    return `${day} ${hour}:${minutes}`;
+}
+
+
+
+
 function displayTemperature(response) {
     console.log(response.data.main.temp);
     let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +32,13 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date-time");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed); 
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt *1000); 
 }
 
 
