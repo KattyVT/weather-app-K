d2e892350ba656a03428e896b3f4a007dc59f554
KattyVT <92695956+KattyVT@users.noreply.github.com>
2021-10-24 16:04:54 +0200

    Added weather data in js file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 function displayTemperature(response) {
     console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement= document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed); 
 }
 
 
 let apiKey = "743bee57fddbfaf52447193a87d5dd25";
 let units = "metric";
-let city = "Frankfurt"
+let city = "Paris"
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 console.log(apiUrl);
